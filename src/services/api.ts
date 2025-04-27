8979f5dc597017286970ec455d4438bce5d4c62f
Tanner Jr., David Arthur <dtannerjr@bwh.harvard.edu>
2025-04-27 16:25:12 -0400

    calling http apit

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://api.nofeed.zone/api", // This will be proxied to the external URL in development
+  baseURL: "http://api.nofeed.zone/api", // This will be proxied to the external URL in development
 })
 
 export default api;
