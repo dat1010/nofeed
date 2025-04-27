d16b38c1399f403ec376a446fbfefc0f38cf9227
David Tanner Jr <tannerdjr@gmail.com>
2025-04-27 18:18:14 -0400

    moved healthcheck

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 // src/App.tsx
 
 import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import HealthCheckPage from "./pages/HealthCheckPage";
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <HealthCheckPage />
+      <BrowserRouter>
+        <Routes>
+          {/* Health check route */}
+          <Route path="/healthcheck" element={<HealthCheckPage />} />
+          
+          {/* Placeholder for the landing page - will be created later */}
+          <Route path="/" element={<div>Landing page coming soon</div>} />
+          
+          {/* Redirect any other routes to the home page */}
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
